f63ec08e32e735dffab120be140ff7be70697a35
Yogin Suttroogun <yo.suttroogun@gmail.com>
2026-04-22 16:55:51 -0400

    Initial Commit

---
+++ ScotiaAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScotiaAssignment
+//
+//  Created by Yogin Kumar Suttroogun on 2026-04-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
